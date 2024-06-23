03f617d21f61d93227b64502dd96f26926a74d57
Kushang Vijay Gharat <53646311+kushang19@users.noreply.github.com>
2024-06-24 02:18:59 +0530

    Redux app

---
+++ src/components/Shop.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch,useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {actionCreators} from '../state/index'
+
+const Shop = () => {
+  const balance = useSelector(state => state.amount)
+  const dispatch = useDispatch();
+  const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators,dispatch)
+  return (
+    <div>
+        <h2>Deposit/Withdraw Money</h2>
+      {/* <button className="btn btn-primary mx-2" onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+      Updated Balance
+      <button className="btn btn-primary mx-2" onClick={() => {dispatch(actionCreators.depositMoney(100))}}>+</button> */}
+      <button className="btn btn-primary mx-2" onClick={() => {withdrawMoney(100)}}>-</button>
+      Updated Balance ({balance})
+      <button className="btn btn-primary mx-2" onClick={() => {depositMoney(100)}}>+</button>
+    </div>
+  )
+}
+
+export default Shop
